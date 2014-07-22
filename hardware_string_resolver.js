1b3203cb5c149fa23d29d3da583a4b73882f09d3
dei79 <dirk.eisenberg@gmail.com>
2014-07-22 22:04:50 +0200

    more marketing style names

--- hardware_string_resolver.js
+++ hardware_string_resolver.js
@@ -10,7 +10,7 @@ function HardwareStringResolver() {}
 HardwareStringResolver.try = function(hwstring) {
 
     var mapping_table = {
-        "iphone1,1": "iPhone (1st generation)",
+        "iphone1,1": "iPhone",
         "iphone1,2": "iPhone 3G",
         "iphone2,1": "iPhone 3GS",
         "iphone3,1": "iPhone 4",
@@ -24,32 +24,32 @@ HardwareStringResolver.try = function(hwstring) {
         "iphone6,1": "iPhone 5S",
         "iphone6,2": "iPhone 5S",
 
-        "ipod1,1": "iPod (1st generation)",
-        "ipod2,1": "iPod (2nd generation)",
-        "ipod3,1": "iPod (3rd generation)",
-        "ipod4,1": "iPod (4th generation)",
-        "ipod5,1": "iPod (5th generation)",
+        "ipod1,1": "iPod 1",
+        "ipod2,1": "iPod 2",
+        "ipod3,1": "iPod 3",
+        "ipod4,1": "iPod 4",
+        "ipod5,1": "iPod 5",
 
-        "ipad1,1": "iPad (1st generation)",
+        "ipad1,1": "iPad",
         "ipad2,1": "iPad 2",
         "ipad2,2": "iPad 2",
         "ipad2,3": "iPad 2",
         "ipad2,4": "iPad 2",
-        "ipad3,1": "iPad (3rd generation)",
-        "ipad3,2": "iPad (3rd generation)",
-        "ipad3,3": "iPad (3rd generation)",
-        "ipad3,4": "iPad (4th generation)",
-        "ipad3,5": "iPad (4th generation)",
-        "ipad3,6": "iPad (4th generation)",
-        "ipad4,1": "iPad (4th generation)",
-        "ipad4,2": "iPad (4th generation)",
-        "ipad4,3": "iPad (4th generation)",
+        "ipad3,1": "iPad 3",
+        "ipad3,2": "iPad 3",
+        "ipad3,3": "iPad 3",
+        "ipad3,4": "iPad 4",
+        "ipad3,5": "iPad 4",
+        "ipad3,6": "iPad 4",
+        "ipad4,1": "iPad Air",
+        "ipad4,2": "iPad Air",
+        "ipad4,3": "iPad Air",
 
-        "ipad2,5": "iPad Mini (1st generation)",
-        "ipad2,6": "iPad Mini (1st generation)",
-        "ipad2,7": "iPad Mini (1st generation)",
-        "ipad4,4": "iPad Mini (1st generation)",
-        "ipad4,5": "iPad Mini (1st generation)"
+        "ipad2,5": "iPad Mini",
+        "ipad2,6": "iPad Mini",
+        "ipad2,7": "iPad Mini",
+        "ipad4,4": "iPad Mini Retina",
+        "ipad4,5": "iPad Mini Retina"
     };
 
     var humanReadableName = mapping_table[hwstring.toLowerCase()];
